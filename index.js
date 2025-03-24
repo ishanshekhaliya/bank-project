0c08619c6c624af623a19dd1f537cce0c3a73051
ishanshekhaliya <ishanshekhaliya.twinnet@gmail.com>
2025-03-24 16:11:49 +0530

    erger

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 
 
 const bankController = require('./Controllers/Bank Controller/bankController')
